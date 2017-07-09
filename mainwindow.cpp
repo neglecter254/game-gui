2f2a7a922f76bbd2f1f9b7a9a6541ee29d4b40de
yuta <mct.d1322@gmail.com>
2017-07-09 09:43:21 +0900

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,16 @@
+//neglecter254@gmail.com x03;BETFto5
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
